823f236bf37a72fc1eadad70be3c7f6e5a821033
Toma Gagne <46379340+TomaGagne@users.noreply.github.com>
2020-10-27 16:11:03 -0400

    fix quantity (#85)

--- reservation-api/src/main/java/ca/ulaval/glo4002/reservation/domain/fullcourse/recipe/illness/IllnessStarter.java
+++ reservation-api/src/main/java/ca/ulaval/glo4002/reservation/domain/fullcourse/recipe/illness/IllnessStarter.java
@@ -8,7 +8,7 @@ import ca.ulaval.glo4002.reservation.domain.fullcourse.Recipe;
 
 public class IllnessStarter extends Recipe {
   private static final BigDecimal SCALLOPS_QUANTITY = BigDecimal.valueOf(2);
-  private static final BigDecimal BUTTERNUT_SQUASH_QUANTITY = BigDecimal.valueOf(8);
+  private static final BigDecimal BUTTERNUT_SQUASH_QUANTITY = BigDecimal.valueOf(4);
 
   public IllnessStarter() {
     ingredients.add(new Ingredient(IngredientName.SCALLOPS, SCALLOPS_QUANTITY));
