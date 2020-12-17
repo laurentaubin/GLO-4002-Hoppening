8b2b214379fd0c5f4da4d7dd2ac2507c818a85b2
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2020-12-16 22:01:06 -0500

    Fix Chef sorting

--- reservation-api/src/main/java/ca/ulaval/glo4002/reservation/api/report/assembler/ChefReportDtoAssembler.java
+++ reservation-api/src/main/java/ca/ulaval/glo4002/reservation/api/report/assembler/ChefReportDtoAssembler.java
@@ -1,6 +1,8 @@
 package ca.ulaval.glo4002.reservation.api.report.assembler;
 
+import java.text.Normalizer;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -31,7 +33,14 @@ public class ChefReportDtoAssembler {
 
   private List<String> getChefsName(Set<Chef> chefs) {
     List<String> chefNames = chefs.stream().map(Chef::getName).collect(Collectors.toList());
-    Collections.sort(chefNames);
+    // inspired from https://stackoverflow.com/questions/26420678/ordering-an-array-with-special-characters-like-accents
+    chefNames.sort(new Comparator<String>() {
+      @Override public int compare(String o1, String o2) {
+        o1 = Normalizer.normalize(o1, Normalizer.Form.NFD);
+        o2 = Normalizer.normalize(o2, Normalizer.Form.NFD);
+        return o1.compareTo(o2);
+      }
+    });
     return chefNames;
   }
 }
